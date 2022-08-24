a57dce7f440fb791a72affc6b4230bf1da87d726
Emilio Devesa <emilio@MacBook-Pro.local>
2022-08-24 18:45:55 +0200

    MainWindow: added some basic labels and a flowbox inside a ScrolledWindow

--- app/gtk/src/window.py
+++ app/gtk/src/window.py
@@ -16,3 +16,12 @@ class MainWindow(Gtk.Window):
         self.set_border_width(15)
         self.set_default_size(640,480)
         self.set_position(Gtk.WindowPosition.CENTER)
+
+        header=Gtk.HeaderBar(title="Compositores")
+        header.set_subtitle("Grandes compositores europeos de los siglos XV a XX")
+        header.props.show_close_button=True
+        self.set_titlebar(header)
+        scrolled=Gtk.ScrolledWindow()
+        scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled.add(self.flowbox)
+        self.add(scrolled)
