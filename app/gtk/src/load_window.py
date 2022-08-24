67ae3575338c6299a6dc92e75c84da085d7dc3cb
Emilio Devesa <emilio@MacBook-Pro.local>
2022-08-24 07:58:04 +0200

    Threaded loading
    We load data from the JSON catalog file and populate an array of items

--- app/gtk/src/load_window.py
+++ app/gtk/src/load_window.py
@@ -3,7 +3,7 @@
 # Emilio Devesa
 # https://emiliodevesa.wordpress.com/
 
-import gi
+import gi, requests, threading, shutil
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk, GLib, GdkPixbuf
 
@@ -24,4 +24,23 @@ class LoadWindow(Gtk.Window):
         self.box.pack_start(self.label, False, False, 0)
         self.box.pack_start(self.spinner, False, False, 0)
         self.add(self.box)
+        self.launch_load()
 
+    def launch_load(self):
+        thread=threading.Thread(target=self.load_json, args=())
+        thread.start()
+    
+    def load_json(self):
+        response=requests.get("https://raw.githubusercontent.com/emilio-devesa/composers/resources/res/catalog.json")
+        json_list=response.json() #Asumimos que la petición se completó con éxito, o fallará si no lo ha hecho
+        result=[]
+        for json_item in json_list:
+            name=json_item.get("name")
+            description=json_item.get("description")
+            image_url=json_item.get("image_url")
+            biography=json.item.get("biography")
+            r=requests.get(image_url,stream=True)
+            with open("temp.png","wb") as f:
+                shutil.copyfileobj(r.raw,f)
+            pixbuf = GdkPixbuf.Pixbuf.new_from_file("temp.png")
+            result.append({"name":name,"description":description,"gtk_image":pixbuf,"biography":biography})
