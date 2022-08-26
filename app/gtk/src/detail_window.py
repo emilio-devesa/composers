95debe5f65fe7ddeab7f7f034e15d88eabfab9d3
Emilio Devesa <emilio@MacBook-Pro.local>
2022-08-26 05:33:40 +0200

    DetailWindow fixes
    Now biography shows adequatly and the window opens at the center of the
    screen. Minor aesthetic adjustments

--- app/gtk/src/detail_window.py
+++ app/gtk/src/detail_window.py
@@ -11,29 +11,24 @@ class DetailWindow(Gtk.Window):
 
     def __init__(self,name,pixbuf,description,biography):
         super().__init__(title=name)
-        self.set_default_size(320,480)
+        self.set_default_size(320,320)
+        self.set_border_width(15)
+        self.set_position(Gtk.WindowPosition.CENTER)
         image=Gtk.Image()
         image.set_from_pixbuf(pixbuf)
 
         # Label adjustments
         biography.set_line_wrap(True)
-        biography.set_justify(Gtk.Justification.RIGHT)
-        biography.set_max_width_chars(40)
+        biography.set_justify(Gtk.Justification.FILL)
 
         # Widgets
-        box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        box.add(image)#, False, False, 0)
-        box.add(description)#, False, False, 0)
-        box.add(biography)#,False,False,20)
+        box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
+        box.set_homogeneous(False)
+        box.pack_start(image, False, False, 10)
+        box.pack_start(description, False, False, 5)
+        box.pack_start(biography, False, False, 0)
         
         scrolled=Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         scrolled.add(box)
-
-        #vbox=Gtk.VBox(False,2)
-        #vbox.pack_start(scrolled, True, True, 0)
-        #box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
-        #box.set_homogeneous(False)
-        #box.pack_start(scrolled, True, True, 0)
-        #self.add(box)
         self.add(scrolled)
