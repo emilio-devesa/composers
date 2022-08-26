bde06fd6a5e460a8634efca4aef95ef0ca9d0254
Emilio Devesa <emilio@MacBook-Pro.local>
2022-08-26 05:18:03 +0200

    DetailWindow biography size
    Now it wraps lines, instead of creating loooooong windows because of
    printing everything in one line. FIXED! :)

--- app/gtk/src/detail_window.py
+++ app/gtk/src/detail_window.py
@@ -14,20 +14,26 @@ class DetailWindow(Gtk.Window):
         self.set_default_size(320,480)
         image=Gtk.Image()
         image.set_from_pixbuf(pixbuf)
-        
+
+        # Label adjustments
+        biography.set_line_wrap(True)
+        biography.set_justify(Gtk.Justification.RIGHT)
+        biography.set_max_width_chars(40)
+
         # Widgets
-        flowbox=Gtk.FlowBox()
-        flowbox.add(image)#, False, False, 0)
-        flowbox.add(description)#, False, False, 0)
-        flowbox.add(biography)#,False,False,20)
+        box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        box.add(image)#, False, False, 0)
+        box.add(description)#, False, False, 0)
+        box.add(biography)#,False,False,20)
         
         scrolled=Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-        scrolled.add(flowbox)
+        scrolled.add(box)
 
         #vbox=Gtk.VBox(False,2)
         #vbox.pack_start(scrolled, True, True, 0)
-        box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
-        box.pack_start(scrolled, True, True, 0)
-        self.add(box)
-        
+        #box=Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=4)
+        #box.set_homogeneous(False)
+        #box.pack_start(scrolled, True, True, 0)
+        #self.add(box)
+        self.add(scrolled)
